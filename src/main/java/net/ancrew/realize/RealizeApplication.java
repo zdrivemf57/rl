f23bbb9521ffe0561415a4063ba4835965f28d58
zdrivemf57 <zdrivemf57@gmail.com>
2022-07-28 14:22:07 +0900

    init

---
+++ src/main/java/net/ancrew/realize/RealizeApplication.java
@@ -0,0 +1,13 @@
+package net.ancrew.realize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealizeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/ancrew/realize/RealizeApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ancrew.realize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
